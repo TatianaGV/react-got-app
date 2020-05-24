71b168f15f49bbfccdfd665e604312d4da864f0b
TatianaGV <tanyanahaus@yandex.ru>
2020-05-25 00:19:37 +0300

    added random character

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-lbolv11nxh">
+            <div className="ldio-uaeoqbdvhwj">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
+
+
